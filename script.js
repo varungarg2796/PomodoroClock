28990e69972b5a789efd95f6b1f150cae9e42ad1
varungarg2796 <varungarg2796@hotmail.com>
2017-04-07 23:34:11 +0530

    Added the countdown

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Some javascript will come here
-var mins, secs;
+var mins, secs,countdown;
 
 
 $(document).ready(function()
@@ -8,6 +8,39 @@ $(document).ready(function()
 	set();
 })
 
+$("#start").on("click", beginTimer);
+
+function beginTimer()
+{
+	init();
+	$("#minutes").text(mins);
+	$("#seperator").text(":");
+	$("#seconds").text(secs);
+	update(); //to start instantly
+	countdown= setInterval(update, 1000);
+}
+
+function update()
+{
+	$("#seconds").text(secs);
+	--secs;
+	if(mins===0 && secs<0)
+	{
+		stop();
+		console.log('Timer Finished');
+	}
+	else if(secs<=0 && mins>0)
+	{
+		secs=59;
+		--mins;
+		$("#minutes").text(mins);
+	}
+}
+
+function stop()
+{
+	clearInterval(countdown);
+}
 
 function init ()
 {
@@ -17,5 +50,5 @@ function init ()
 
 function set()
 {
-	$(".timer").text(mins);
+	$("#minutes").text(mins);
 }
