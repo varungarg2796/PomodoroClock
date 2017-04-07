2c9eecc5ba031e7e3bbd206a792701e5ca34b274
varungarg2796 <varungarg2796@hotmail.com>
2017-04-07 23:10:56 +0530

    Added the timer

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //Some javascript will come here
+var mins, secs;
+
+
+$(document).ready(function()
+{
+	init();
+	set();
+})
+
+
+function init ()
+{
+	mins=25;
+	secs=59; 
+}
+
+function set()
+{
+	$(".timer").text(mins);
+}
